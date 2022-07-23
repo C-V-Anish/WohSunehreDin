9d23d699654ac7c1c0e09036d1695d63e9d729d1
C-V-Anish <mahadevrao77@gmail.com>
2022-07-23 16:56:58 +0530

    HomePage WIP

--- OldEra/urls.py
+++ OldEra/urls.py
@@ -1,6 +1,10 @@
+from cv2 import namedWindow
 from django.urls import path
 from . import views
 
 urlpatterns = [
-path('',views.index,name="home")
+path('',views.index,name="home"),
+path('movies/',views.movies,name="movies"),
+path('songs/',views.songs,name="songs"),
+path('aboutus/',views.aboutus,name='aboutus')
 ]
--- OldEra/views.py
+++ OldEra/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def movies(request):
+    return HttpResponse("Movies Page")
+
+def songs(request):
+    return HttpResponse("Songs Page")
+
+def aboutus(request):
+    return HttpResponse("About Page")
